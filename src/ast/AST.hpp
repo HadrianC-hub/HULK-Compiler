994f6ffabbd5f68347eb76a0988c13ad5d774926
Adrián Hernández Castellanos <hadrianc02@gmail.com>
2025-04-18 19:54:49 -0400

    Nodos de operaciones restantes del AST

--- src/ast/AST.hpp
+++ src/ast/AST.hpp
@@ -57,6 +57,7 @@ public:
     std::string type() const override { return _type; }
 };
 
+// ------------------------------------------------------
 
 class FunctionCallNode : public ASTNode {
 public:
@@ -72,3 +73,70 @@ public:
     std::string type() const override { return _type; }
 };
 
+// ------------------------------------------------------
+
+class LiteralNode : public ASTNode {
+public:
+    std::string value;
+    std::string _type;
+    int _line;
+
+    LiteralNode(std::string val, std::string type, int ln)
+        : value(val), _type(type), _line(ln) {}
+
+    int line() const override { return _line; }
+    std::string type() const override { return _type; }
+};
+
+// ------------------------------------------------------
+
+class BlockNode : public ASTNode {
+public:
+    std::vector<ASTNode*> expressions;
+    int _line;
+    std::string _type;
+
+    BlockNode(std::vector<ASTNode*> exprs, int ln)
+        : expressions(exprs), _line(ln), _type("") {}
+
+    int line() const override { return _line; }
+    std::string type() const override { return _type; }
+};
+
+// ------------------------------------------------------
+
+class VariableDeclarationNode : public ASTNode {
+public:
+    std::string name;
+    std::string declaredType;
+    ASTNode* initializer;
+    bool isMutable;
+    int _line;
+    std::string _type;
+
+    VariableDeclarationNode(std::string name, std::string type, ASTNode* init, bool isMut, int ln)
+        : name(name), declaredType(type), initializer(init), isMutable(isMut), _line(ln), _type("") {}
+
+    int line() const override { return _line; }
+    std::string type() const override { return _type; }
+};
+
+// ------------------------------------------------------
+
+class IdentifierNode : public ASTNode {
+public:
+    std::string name;
+    int _line;
+    std::string _type;
+
+    IdentifierNode(std::string name, int ln)
+        : name(name), _line(ln), _type("") {}
+
+    int line() const override { return _line; }
+    std::string type() const override { return _type; }
+};
+
+struct Parameter {
+    std::string name;
+    std::string type;
+};
