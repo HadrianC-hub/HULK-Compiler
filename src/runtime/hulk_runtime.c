3061eda6883cb49d9b14ef5418ea1b6b8619acb4
Adrián Hernández Castellanos <hadrianc02@gmail.com>
2025-04-23 14:13:09 -0400

    Implementación de la función de concatenación por espacios (runtime)

--- src/runtime/hulk_runtime.c
+++ src/runtime/hulk_runtime.c
@@ -14,3 +14,13 @@ char *hulk_str_concat(const char *a, const char *b)
     return res;
 }
 
+// a + " " + b → "a b"
+char *hulk_str_concat_space(const char *a, const char *b)
+{
+    size_t len = strlen(a) + strlen(b) + 2;
+    char *res = malloc(len);
+    strcpy(res, a);
+    strcat(res, " ");
+    strcat(res, b);
+    return res;
+}
