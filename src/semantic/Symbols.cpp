b5dc869af551a9125c8f4e03a20eded3ce2e7121
LauraMarby <lauramartir2@gmail.com>
2025-05-01 18:52:59 -0400

    Implementadas funciones de agregar simbolos y de buscar al padre en un ambito local

--- src/semantic/Symbols.cpp
+++ src/semantic/Symbols.cpp
@@ -20,9 +20,22 @@ void SymbolTable::exitScope() {
     if (!scopes.empty()) scopes.pop_back();
 }
 
+bool SymbolTable::addSymbol(const std::string& name, const std::string& type, bool is_const, const std::vector<std::string>& params) {
+    if (scopes.empty()) return false;
+    auto& current = scopes.back();
+    if (current.find(name) != current.end()) return false;
+    current[name] = Symbol{"variable", type, is_const, params};
+    return true;
+}
 
-
-
+Symbol* SymbolTable::lookup(const std::string& name) {
+    // Buscar en ámbitos locales
+    for (auto it = scopes.rbegin(); it != scopes.rend(); ++it) {
+        auto entry = it->find(name);
+        if (entry != it->end()) return &entry->second;
+    }
+    return nullptr;
+}
 
 
 
