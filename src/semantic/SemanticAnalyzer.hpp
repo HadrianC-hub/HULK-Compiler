07335218eaaecfe90d4dfd9bd25bfc52d3624de4
Yesenia Valdés Rodríguez <96796931+yesi07111@users.noreply.github.com>
2025-05-16 20:31:55 -0500

    refactorizar el header de semantica y agregar funciones faltantes

--- src/semantic/SemanticAnalyzer.hpp
+++ src/semantic/SemanticAnalyzer.hpp
@@ -4,27 +4,43 @@
 #include "../utils/SemanticError.hpp"
 #include <vector>
 
-class SemanticAnalyzer : public ASTVisitor
-{
+class SemanticAnalyzer : public ASTVisitor {
 private:
     SymbolTable symbolTable;
+    
+    void checkMathFunction(const std::string& funcName, const std::vector<ASTNode*>& args, int line);
+
+    bool isValidIdentifier(const std::string& name);
+
+    bool conformsTo(const std::string& subtype, const std::string& supertype);
+
     std::vector<SemanticError> errors;
 
 public:
-    void analyze(const std::vector<ASTNode *> &nodes);
-    SymbolTable &getSymbolTable();
+    void analyze(const std::vector<ASTNode*>& nodes);
 
-    void visit(ASTNode &node) override {}
-    void visit(LiteralNode &node) override;
-    void visit(IdentifierNode &node) override;
-    void visit(VariableDeclarationNode &node) override;
-    void visit(BinaryOperationNode &node) override;
-    void visit(IfNode &node) override;
-    void visit(WhileNode &node) override;
-    void visit(BlockNode &node) override;
-    void visit(UnaryOpNode &node) override;
+    void visit(FunctionDeclarationNode& node) override;
+    void visit(BinaryOperationNode& node) override;
+    void visit(FunctionCallNode& node) override;
+    void visit(ASTNode& node) override;
+    void visit(LiteralNode& node) override;
+    void visit(BlockNode& node) override;
+    void visit(VariableDeclarationNode& node) override;
+    void visit(IdentifierNode& node) override;
+    void visit(LetNode& node) override;
+    void visit(AssignmentNode& node) override;
+    void visit(IfNode& node) override;
+    void visit(WhileNode& node) override;
+    void visit(ForNode& node) override;
+    void visit(TypeDeclarationNode& node) override;
+    void visit(NewInstanceNode& node) override;
+    void visit(UnaryOpNode& node) override;
+    void visit(BuiltInFunctionNode& node) override;
+    void visit(MethodCallNode& node) override;
+    // void visit(AttributeDeclaration& node) override;
+    // void visit(MethodDeclaration& node) override;
 
-    bool conformsTo(const std::string &subtype, const std::string &supertype);
+    const std::vector<SemanticError>& getErrors() const { return errors; }
 
-    const std::vector<SemanticError> &getErrors() const { return errors; }
+    SymbolTable& getSymbolTable();
 };
