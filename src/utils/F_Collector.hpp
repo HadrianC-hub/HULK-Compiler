f9492d5ef15dd3190b05b55e38da405eb126db55
Yesenia Valdés Rodríguez <96796931+yesi07111@users.noreply.github.com>
2025-05-17 20:49:27 -0500

    actualizando function collector header con funciones a implementar

--- src/utils/F_Collector.hpp
+++ src/utils/F_Collector.hpp
@@ -17,4 +17,22 @@ public:
 
     void visit(ASTNode &node) override {}
     void visit(FunctionDeclarationNode &node) override;
+    void visit(BinaryOperationNode &node) override {}
+    void visit(FunctionCallNode &node) override {}
+    void visit(LiteralNode &node) override {}
+    void visit(BlockNode &node) override {}
+    void visit(VariableDeclarationNode &node) override {}
+    void visit(IdentifierNode &node) override {}
+    void visit(LetNode &node) override {}
+    void visit(AssignmentNode &node) override {}
+    void visit(IfNode &node) override {}
+    void visit(WhileNode &node) override {}
+    void visit(ForNode &node) override {}
+    void visit(TypeDeclarationNode &node) override {}
+    void visit(NewInstanceNode &node) override {}
+    void visit(UnaryOpNode &node) override {}
+    void visit(BuiltInFunctionNode &node) override {}
+    void visit(MethodCallNode &node) override {}
+    // void visit(AttributeDeclaration& node) override {}
+    // void visit(MethodDeclaration& node) override {}
 };
