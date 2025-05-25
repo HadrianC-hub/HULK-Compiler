d3538df00fc12b902a6383910aaf606781fe74e7
Yesenia Valdés Rodríguez <96796931+yesi07111@users.noreply.github.com>
2025-05-24 19:14:08 -0500

    Agregando funciones auxiliares de scope y funciones

--- src/codegen/Context.hpp
+++ src/codegen/Context.hpp
@@ -23,4 +23,44 @@ public:
     CodeGenContext();
 
     void generateCode(std::vector<ASTNode *> &root);
-}
+    void dumpIR(const std::string &filename = "hulk-low-code.ll");
+
+    std::vector<std::map<std::string, llvm::Value *>> localScopes;
+    std::vector<std::map<std::string, FunctionDeclarationNode *>> functionScopes;
+
+    void pushVarScope() { localScopes.emplace_back(); }
+    void popVarScope() { localScopes.pop_back(); }
+    void addLocal(const std::string &name, llvm::Value *val)
+    {
+        if (!localScopes.empty())
+            localScopes.back()[name] = val;
+    }
+    llvm::Value *lookupLocal(const std::string &name) const
+    {
+        for (auto it = localScopes.rbegin(); it != localScopes.rend(); ++it)
+        {
+            auto found = it->find(name);
+            if (found != it->end())
+                return found->second;
+        }
+        return nullptr;
+    }
+
+    void pushFuncScope() { functionScopes.emplace_back(); }
+    void popFuncScope() { functionScopes.pop_back(); }
+    void addFuncDecl(const std::string &name, FunctionDeclarationNode *decl)
+    {
+        if (!functionScopes.empty())
+            functionScopes.back()[name] = decl;
+    }
+    FunctionDeclarationNode *lookupFuncDecl(const std::string &name) const
+    {
+        for (auto it = functionScopes.rbegin(); it != functionScopes.rend(); ++it)
+        {
+            auto found = it->find(name);
+            if (found != it->end())
+                return found->second;
+        }
+        return nullptr;
+    }
+};
