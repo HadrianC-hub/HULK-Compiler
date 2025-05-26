4ca7e9817d3fd04e5cf53fd6f810957eb0c622f1
LauraMarby <lauramartir2@gmail.com>
2025-05-26 14:17:39 -0400

    iniciando generacion de codigo llvm

---
+++ src/codegen/LLVM.cpp
@@ -0,0 +1,71 @@
+#include "LLVM.hpp"
+#include "Context.hpp"
+#include "../ast/AST.hpp"
+#include <stdexcept>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <math.h>
+
+extern "C"
+{
+    char *hulk_str_concat(const char *a, const char *b);
+    char *hulk_str_concat_space(const char *a, const char *b);
+    bool hulk_str_equals(const char *a, const char *b);
+    double hulk_log_base_checked(double x, double base);
+    double hulk_rand();
+}
+
+LLVMGenerator::LLVMGenerator(CodeGenContext &ctx)
+    : context(ctx) {}
+
+std::string processRawString(const std::string &raw)
+{
+    std::string processed;
+    bool escape = false;
+    size_t start = (raw.front() == '"') ? 1 : 0;
+    size_t end = (raw.back() == '"') ? raw.size() - 1 : raw.size();
+
+    for (size_t i = start; i <= end; ++i)
+    {
+        char c = raw[i];
+        if (escape)
+        {
+            switch (c)
+            {
+            case 'n':
+                processed += '\n';
+                break;
+            case 't':
+                processed += '\t';
+                break;
+            case '"':
+                processed += '"';
+                break;
+            case '\\':
+                processed += '\\';
+                break;
+            default:
+                processed += c;
+            }
+            escape = false;
+        }
+        else if (c == '\\')
+        {
+            escape = true;
+        }
+        else
+        {
+            processed += c;
+        }
+    }
+
+    if (escape)
+    {
+        processed += '\\';
+    }
+
+    return processed;
+}
+
+
