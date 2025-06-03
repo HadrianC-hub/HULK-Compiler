a8099a688b88250b820db585e1ba6b0d6c841212
LauraMarby <lauramartir2@gmail.com>
2025-06-03 15:38:07 -0400

    generacion de codigo: operacion binaria

--- src/codegen/LLVM.cpp
+++ src/codegen/LLVM.cpp
@@ -101,4 +101,145 @@ void LLVMGenerator::visit(LiteralNode &node)
     std::cout << "[PROC CG] Emitido literal de tipo " << node._type << std::endl;
 }
 
+void LLVMGenerator::visit(BinaryOperationNode &node)
+{
+    node.left->accept(*this);
+    llvm::Value *left = context.valueStack.back();
+    context.valueStack.pop_back();
+    node.right->accept(*this);
+    llvm::Value *right = context.valueStack.back();
+    context.valueStack.pop_back();
+
+    llvm::Value *result = nullptr;
+    llvm::IRBuilder<> &builder = context.builder;
+
+    const std::string &op = node.op;
+
+    if (op == "+")
+    {
+        result = builder.CreateFAdd(left, right, "addtmp");
+    }
+    else if (op == "-")
+    {
+        result = builder.CreateFSub(left, right, "subtmp");
+    }
+    else if (op == "*")
+    {
+        result = builder.CreateFMul(left, right, "multmp");
+    }
+    else if (op == "/")
+    {
+        result = builder.CreateFDiv(left, right, "divtmp");
+    }
+    else if (op == "%")
+    {
+        result = builder.CreateFRem(left, right, "modtmp");
+    }
+    else if (op == "^")
+    {
+        llvm::Function *powFn = context.module.getFunction("llvm.pow.f64");
+        if (!powFn)
+        {
+            llvm::FunctionType *powType = llvm::FunctionType::get(
+                llvm::Type::getDoubleTy(context.context),
+                {llvm::Type::getDoubleTy(context.context), llvm::Type::getDoubleTy(context.context)},
+                false);
+            powFn = llvm::Function::Create(powType, llvm::Function::ExternalLinkage, "llvm.pow.f64", context.module);
+        }
+        result = builder.CreateCall(powFn, {left, right}, "powtmp");
+    }
+
+    else if (op == "<")
+    {
+        result = builder.CreateFCmpULT(left, right, "lttmp");
+    }
+    else if (op == "<=")
+    {
+        result = builder.CreateFCmpULE(left, right, "letmp");
+    }
+    else if (op == ">")
+    {
+        result = builder.CreateFCmpUGT(left, right, "gttmp");
+    }
+    else if (op == ">=")
+    {
+        result = builder.CreateFCmpUGE(left, right, "getmp");
+    }
+    else if (op == "==" || op == "!=")
+    {
+        const std::string &nodeType = node.left->type();
+
+        if (nodeType == "Number")
+        {
+            result = (op == "==")
+                         ? builder.CreateFCmpUEQ(left, right, "eqtmp")
+                         : builder.CreateFCmpUNE(left, right, "netmp");
+        }
+        else if (nodeType == "Boolean")
+        {
+            result = (op == "==")
+                         ? builder.CreateICmpEQ(left, right, "beqtmp")
+                         : builder.CreateICmpNE(left, right, "bnetmp");
+        }
+        else if (nodeType == "String")
+        {
+            llvm::Function *eqFn = context.module.getFunction("hulk_str_equals");
+            if (!eqFn)
+            {
+                llvm::FunctionType *eqType = llvm::FunctionType::get(
+                    llvm::Type::getInt1Ty(context.context),
+                    {llvm::PointerType::get(llvm::Type::getInt8Ty(context.context), 0),
+                     llvm::PointerType::get(llvm::Type::getInt8Ty(context.context), 0)},
+                    false);
+                eqFn = llvm::Function::Create(
+                    eqType, llvm::Function::ExternalLinkage,
+                    "hulk_str_equals", context.module);
+            }
+
+            llvm::Value *eqCall = builder.CreateCall(eqFn, {left, right}, "strequal");
+
+            result = (op == "!=")
+                         ? builder.CreateNot(eqCall, "strnotequal")
+                         : eqCall;
+        }
+        else
+        {
+            throw std::runtime_error("[ERROR CG] Tipo no soportado para operaciones '==' o '!=': " + nodeType);
+        }
+    }
+
+    else if (op == "&")
+    {
+        result = builder.CreateAnd(left, right, "andtmp");
+    }
+    else if (op == "|")
+    {
+        result = builder.CreateOr(left, right, "ortmp");
+    }
+
+    else if (op == "@" || op == "@@")
+    {
+        const char *funcName = (op == "@") ? "hulk_str_concat" : "hulk_str_concat_space";
+
+        llvm::Function *concatFn = context.module.getFunction(funcName);
+        if (!concatFn)
+        {
+            llvm::FunctionType *concatType = llvm::FunctionType::get(
+                llvm::PointerType::get(llvm::Type::getInt8Ty(context.context), 0),
+                {llvm::PointerType::get(llvm::Type::getInt8Ty(context.context), 0), llvm::PointerType::get(llvm::Type::getInt8Ty(context.context), 0)},
+                false);
+            concatFn = llvm::Function::Create(concatType, llvm::Function::ExternalLinkage, funcName, context.module);
+        }
+
+        result = context.builder.CreateCall(concatFn, {left, right}, "concat");
+    }
+
+    else
+    {
+        throw std::runtime_error("[ERROR CG] Operador binario no soportado: " + op);
+    }
+
+    context.valueStack.push_back(result);
+    std::cout << "[PROC CG] Operación binaria '" << op << "' emitida.\n";
+}
 
